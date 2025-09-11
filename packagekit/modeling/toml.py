4eb2515c51b2142bcec8c8b927b2327c774d932c
mm21 <mm21.dev@gmail.com>
2025-09-11 12:08:31 -0700

    feat: implement toml primitive arrays

--- packagekit/modeling/toml.py
+++ packagekit/modeling/toml.py
@@ -4,49 +4,136 @@ Layer to map TOML files (via `tomlkit`) to/from Pydantic models.
 
 from __future__ import annotations
 
+from abc import ABC, abstractmethod
+from datetime import date as Date
+from datetime import datetime as DateTime
+from datetime import time as Time
 from pathlib import Path
-from typing import Any, Mapping, Self
+from typing import Any, Mapping, Self, get_origin
 
 import tomlkit
 import tomlkit.items
-from pydantic import BaseModel, ConfigDict, PrivateAttr, ValidationInfo, field_validator
+from pydantic import BaseModel, ConfigDict, ValidationInfo, field_validator
+
+from ..typing.generics import extract_type_param
+
+type PrimitiveType = str | int | float | bool | Date | Time | DateTime
+"""
+Primitive type which can be used as a container field.
+"""
 
 
-class BaseTomlModel(BaseModel):
-    model_config = ConfigDict(extra="forbid")
-
-
-class BaseContainer[ContainerT: Mapping](BaseTomlModel):
+class BaseTomlElement[TomlkitT](ABC):
     """
-    Container for items in a document or table. Handles validation and serialization,
-    coercing to/from `BaseTable` subclasses and `tomlkit` types like
-    `tomlkit.items.Array`.
+    Abstracts a TOML element corresponding to a type from `tomlkit`.
     """
 
-    __tomlkit_container: ContainerT = PrivateAttr()
+    _tomlkit_cls: type[TomlkitT]
+    """
+    Corresponding class from `tomlkit`.
+    """
+
+    _tomlkit_obj: TomlkitT
+    """
+    Corresponding object from `tomlkit`, either extracted upon load or newly created.
+    """
+
+    def __init_subclass__(cls):
+        super().__init_subclass__()
+
+        if hasattr(cls, "_tomlkit_cls"):
+            return
+
+        if tomlkit_cls := extract_type_param(cls, BaseTomlElement):
+            cls._tomlkit_cls = tomlkit_cls
+
+    @classmethod
+    @abstractmethod
+    def _coerce(cls, tomlkit_obj: TomlkitT) -> Self: ...
+
+    @classmethod
+    def _create(cls, tomlkit_obj: TomlkitT) -> Self:
+        obj = cls._coerce(tomlkit_obj)
+        obj._tomlkit_obj = tomlkit_obj
+        return obj
+
+
+class BasePrimitiveArray[ItemT: PrimitiveType](
+    list[ItemT], BaseTomlElement[tomlkit.items.Array]
+):
+    """
+    Base array of primitive types.
+    """
+
+    _default_multiline: bool | None = None
+    """
+    Default value for `tomlkit`'s multiline state for this array, only applicable if
+    newly created.
+    """
+
+    @classmethod
+    def _coerce(cls, tomlkit_obj: tomlkit.items.Array) -> Self:
+        return cls(tomlkit_obj.value)
+
+
+class PrimitiveArray[ItemT: PrimitiveType](BasePrimitiveArray[ItemT]):
+    """
+    Array of primitive types, using the multiline state from the underlying `tomlkit`
+    object.
+    """
+
+
+class MultilinePrimitiveArray[ItemT: PrimitiveType](PrimitiveArray[ItemT]):
+    """
+    Array of primitive types, ensuring the multiline state from the underlying `tomlkit`
+    object is enabled.
+    """
+
+    _default_multiline = True
+
+
+# TODO: validate all fields upon class creation
+class BaseContainer[TomlkitT: Mapping](BaseModel, BaseTomlElement[TomlkitT]):
+    """
+    Container for items in a document or table. Upon reading a TOML file via
+    `tomlkit`, coerces values from `tomlkit` types to the corresponding class
+    in this package.
+
+    Only primitives (`str`, `int`, `float`, `bool`, `date`, `time`, `datetime`)
+    and `BaseTomlElement` subclasses are allowed as fields.
+    """
+
+    model_config = ConfigDict(extra="forbid", arbitrary_types_allowed=True)
 
     @field_validator("*", mode="before", check_fields=False)
     @classmethod
-    def validate_field(cls, val: Any, info: ValidationInfo) -> Any:
+    def validate_field(cls, value: Any, info: ValidationInfo) -> Any:
         assert info.field_name
 
         field = cls.model_fields.get(info.field_name)
         assert field
 
-        field_type = field.annotation
-        assert field_type
+        annotation = field.annotation
+        assert annotation
 
-        if issubclass(field_type, BaseTable):
-            assert isinstance(val, tomlkit.items.Table)
-            return cls._coerce_table(val, field_type)
-        if issubclass(field_type, BaseInlineTable):
-            assert isinstance(val, tomlkit.items.InlineTable)
-            return cls._coerce_inline_table(val, field_type)
+        origin = get_origin(annotation)
 
-        return val
+        if origin:
+            field_type = origin
+        else:
+            field_type = annotation
+
+        # coerce value if type is element
+        if issubclass(field_type, BaseTomlElement):
+            assert isinstance(value, field_type._tomlkit_cls)  # type: ignore
+            return field_type._create(value)
+
+        # TODO: sanity check: ensure field is primitive
+
+        return value
 
     @classmethod
-    def _from_container(cls, container: ContainerT) -> Self:
+    def _coerce(cls, tomlkit_obj: TomlkitT) -> Self:
         """
         Extract model fields from container and return instance of this model with
         the original container stored.
@@ -54,39 +141,10 @@ class BaseContainer[ContainerT: Mapping](BaseTomlModel):
         fields: dict[str, Any] = {}
 
         for name in cls.model_fields.keys():
-            if name in container:
-                fields[name] = container[name]
+            if name in tomlkit_obj:
+                fields[name] = tomlkit_obj[name]
 
-        obj = cls(**fields)
-        obj._set_container(container)
-
-        return obj
-
-    @classmethod
-    def _coerce_table[T: BaseTable](
-        cls, val: tomlkit.items.Table, table_cls: type[T]
-    ) -> T:
-        return table_cls._from_container(val)
-
-    @classmethod
-    def _coerce_inline_table[T: BaseInlineTable](
-        cls, val: tomlkit.items.InlineTable, inline_table_cls: type[T]
-    ) -> T:
-        # TODO
-        ...
-
-    def _set_container(self, container: ContainerT):
-        """
-        Set tomlkit container for later use.
-        """
-        self.__tomlkit_container = container
-
-    def _update_container(self):
-        """
-        Update tomlkit container with values from model, preserving any existing values.
-        """
-        # TODO
-        ...
+        return cls(**fields)
 
 
 class BaseDocument(BaseContainer[tomlkit.TOMLDocument]):
@@ -107,7 +165,7 @@ class BaseDocument(BaseContainer[tomlkit.TOMLDocument]):
 
     @classmethod
     def loads(cls, string: str, /) -> Self:
-        return cls._from_container(tomlkit.loads(string))
+        return cls._create(tomlkit.loads(string))
 
     def dump(self, file: Path, /):
         # TODO
--- test/modeling/test_toml.py
+++ test/modeling/test_toml.py
@@ -1,11 +1,16 @@
 from __future__ import annotations
 
-from packagekit.modeling.toml import BaseDocument, BaseTable
+from packagekit.modeling.toml import (
+    BaseDocument,
+    BaseTable,
+    PrimitiveArray,
+)
 
 
 class DocumentTest(BaseDocument):
     string_test: str
     int_test: int
+    array_test: PrimitiveArray[int]
     table_test: TableTest
 
 
@@ -16,6 +21,7 @@ class TableTest(BaseTable):
 DOCUMENT_STR = """
 string_test = "test string"
 int_test = 123
+array_test = [1, 2, 3]
 
 [table_test]
 table_test_str = "table test string"
@@ -23,8 +29,10 @@ table_test_str = "table test string"
 
 
 def test_document():
+
     document = DocumentTest.loads(DOCUMENT_STR)
 
     assert document.string_test == "test string"
     assert document.int_test == 123
+    assert document.array_test == [1, 2, 3]
     assert document.table_test.table_test_str == "table test string"
