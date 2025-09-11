8e5c8b2ba1a2e2e623d2ae44a80e6de06fe2a831
mm21 <mm21.dev@gmail.com>
2025-09-11 12:34:15 -0700

    refactor(typing): make origin, arg checks more robust

--- packagekit/typing/generics.py
+++ packagekit/typing/generics.py
@@ -1,4 +1,4 @@
-from typing import TypeVar, get_args, get_origin, overload
+from typing import Any, TypeVar, get_args, get_origin, overload
 
 
 @overload
@@ -15,43 +15,48 @@ def extract_type_param[BaseT, ParamT](
     cls, base_cls: type[BaseT], param_base_cls: type[ParamT] | None = None
 ) -> type[ParamT] | type | None:
     """
-    Extract the concrete type param with the given class. If `base_cls` can be
+    Extract the concrete type param from the given base class. If `base_cls` can be
     parameterized with multiple types, it's recommend to also pass `param_base_cls`
     to get the desired type param.
     """
 
-    # check for pydantic model
+    def check_origin(origin: Any) -> bool:
+        return (
+            origin is not None
+            and isinstance(origin, type)
+            and issubclass(origin, base_cls)
+        )
+
+    def check_arg(arg: Any) -> bool:
+        if arg is None or not isinstance(arg, type):
+            return False
+        elif param_base_cls and not issubclass(arg, param_base_cls):
+            return False
+        else:
+            return True
+
+    # check for pydantic model first
     if pydantic_generic_metadata := getattr(cls, "__pydantic_generic_metadata__", None):
         origin = pydantic_generic_metadata["origin"]
-        if origin is not None and issubclass(origin, base_cls):
+        if check_origin(origin):
             for arg in pydantic_generic_metadata["args"]:
-                if isinstance(arg, type):
+                if check_arg(arg):
                     return arg
 
     orig_bases = getattr(cls, "__orig_bases__", ())
-
     for base in orig_bases:
         origin = get_origin(base)
-
-        # skip if not desired base class
-        if (
-            not origin
-            or not isinstance(origin, type)
-            or not issubclass(origin, base_cls)
-        ):
+        if not check_origin(origin):
             continue
 
         for arg in get_args(base):
 
-            # TODO: get TypeVar bound, but prioritize concrete type?
+            # TODO: check TypeVar bound, but prioritize concrete type?
             if isinstance(arg, TypeVar):
                 continue
 
-            if not isinstance(arg, type):
-                continue
-
-            if param_base_cls and not issubclass(arg, param_base_cls):
-                continue
+            if check_arg(arg):
+                return arg
 
             return arg
 
