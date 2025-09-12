8714a5d563923a9627a3cb8a8bb3b101970693b4
mm21 <mm21.dev@gmail.com>
2025-09-12 11:22:15 -0700

    chore(typing): cleanup

--- packagekit/typing/generics.py
+++ packagekit/typing/generics.py
@@ -2,26 +2,25 @@ from typing import Any, TypeVar, cast, get_args, get_origin, overload
 
 
 @overload
-def get_type_param[BaseT](cls: type, base_cls: type[BaseT]) -> type | None: ...
+def get_type_param[BaseT](cls: type[Any], base_cls: type[BaseT]) -> type | None: ...
 
 
 @overload
 def get_type_param[BaseT, ParamT](
-    cls: type, base_cls: type[BaseT], param_base_cls: type[ParamT]
+    cls: type[Any], base_cls: type[BaseT], param_base_cls: type[ParamT]
 ) -> type[ParamT] | None: ...
 
 
+# TODO: pass index of desired param to differentiate multiple type params of the
+# same type
 def get_type_param[BaseT, ParamT](
-    cls: type, base_cls: type[BaseT], param_base_cls: type[ParamT] | None = None
+    cls: type[Any], base_cls: type[BaseT], param_base_cls: type[ParamT] | None = None
 ) -> type[ParamT] | type | None:
     """
     Extract the concrete type param from the given base class. If `base_cls` can be
     parameterized with multiple types, it's recommend to also pass `param_base_cls`
     to get the desired type param.
     """
-
-    def get_bases(cls: type, attr: str) -> list[type]:
-        return list(cast(tuple[type], getattr(cls, attr, ())))
 
     def check_origin(origin: Any) -> bool:
         return (
@@ -38,26 +37,18 @@ def get_type_param[BaseT, ParamT](
         else:
             return True
 
-    def check_cls(cls: type) -> type[ParamT] | type | None:
+    def get_bases(cls: type, attr: str) -> list[type]:
+        return list(cast(tuple[type], getattr(cls, attr, ())))
 
-        # check for pydantic model first
-        if pydantic_generic_metadata := getattr(
-            cls, "__pydantic_generic_metadata__", None
-        ):
-            origin = pydantic_generic_metadata["origin"]
-            if check_origin(origin):
-                for arg in pydantic_generic_metadata["args"]:
-                    if check_arg(arg):
-                        return arg
+    def get_arg(cls: type) -> type[ParamT] | type | None:
+        # check pydantic metadata if applicable
+        if metadata := getattr(cls, "__pydantic_generic_metadata__", None):
+            origin, args = metadata["origin"], metadata["args"]
+        else:
+            origin, args = get_origin(cls), get_args(cls)
 
-        bases = get_bases(cls, "__orig_bases__") + get_bases(cls, "__bases__")
-        for base in bases:
-            origin = get_origin(base)
-            if not check_origin(origin):
-                continue
-
-            for arg in get_args(base):
-
+        if check_origin(origin):
+            for arg in args:
                 # TODO: check TypeVar bound, but prioritize concrete type?
                 if isinstance(arg, TypeVar):
                     continue
@@ -65,11 +56,11 @@ def get_type_param[BaseT, ParamT](
                 if check_arg(arg):
                     return arg
 
-        # not found, recurse into each base
-        for base in bases:
-            if param := check_cls(base):
+        # not found, recurse into bases
+        for base in get_bases(cls, "__orig_bases__") + get_bases(cls, "__bases__"):
+            if param := get_arg(base):
                 return param
 
         return None
 
-    return check_cls(cls)
+    return get_arg(cls)
