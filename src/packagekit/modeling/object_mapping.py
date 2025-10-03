dcd02a79b0bf48fe39febb24f45e08ef6cad1d62
mm21 <mm21.dev@gmail.com>
2025-10-02 23:30:15 -0700

    refactor(object_collections): maintain key object references via dict

--- src/packagekit/modeling/object_mapping.py
+++ src/packagekit/modeling/object_mapping.py
@@ -1,10 +1,14 @@
 from typing import Iterator, MutableMapping
 
+__all__ = [
+    "ObjectMapping",
+]
+
 
 class ObjectMapping[KT, VT](MutableMapping[KT, VT]):
     """
     Maintains a mapping from arbitrary objects to other objects using id of keys to
-    support non-hashable keys. Keeps a list of the key objects to ensure they don't
+    support non-hashable keys. Keeps a mapping of the key objects to ensure they don't
     get garbage collected.
     """
 
@@ -13,14 +17,14 @@ class ObjectMapping[KT, VT](MutableMapping[KT, VT]):
     Mapping of key id to object.
     """
 
-    __key_list: list[KT]
+    __key_map: dict[int, KT]
     """
-    List of key objects.
+    Mapping of key id to key object, used to maintain its reference count.
     """
 
     def __init__(self):
         self.__id_map = {}
-        self.__key_list = []
+        self.__key_map = {}
 
     def __getitem__(self, key: KT) -> VT:
         key_id = id(key)
@@ -30,38 +34,31 @@ class ObjectMapping[KT, VT](MutableMapping[KT, VT]):
 
     def __setitem__(self, key: KT, value: VT) -> None:
         key_id = id(key)
-
-        # if key doesn't exist, add it to item list to preserve reference
-        if key_id not in self.__id_map:
-            self.__key_list.append(key)
-
         self.__id_map[key_id] = value
+        self.__key_map[key_id] = key
 
     def __delitem__(self, key: KT) -> None:
         key_id = id(key)
 
-        if key_id not in self.__id_map:
+        if key_id not in self.__id_map or key_id not in self.__key_map:
             raise KeyError(key)
 
-        # remove from id mapping
         del self.__id_map[key_id]
-
-        # remove from item list
-        for i, item in enumerate(self.__key_list):
-            if id(item) == key_id:
-                del self.__key_list[i]
-                break
+        del self.__key_map[key_id]
 
     def __iter__(self) -> Iterator[KT]:
-        return iter(self.__key_list)
+        return iter(self.__key_map.values())
 
     def __len__(self) -> int:
-        return len(self.__key_list)
+        return len(self.__id_map)
 
     def __repr__(self) -> str:
-        items = [(key, self.__id_map[id(key)]) for key in self.__key_list]
+        items = {
+            str(key): str(self.__id_map[key_id])
+            for key_id, key in self.__key_map.items()
+        }
         return f"{self.__class__.__name__}({dict(items)})"
 
     def clear(self) -> None:
         self.__id_map.clear()
-        self.__key_list.clear()
+        self.__key_map.clear()
